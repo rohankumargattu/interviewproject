74c0103bf56fbba4dc7a352dd06a1ee221f21fb3
gattu <gattu@DESKTOP-D8HNM4S>
2020-11-04 21:40:11 +0530

    2 new programs

---
+++ src/test/java/apiprograms/Test1.java
@@ -0,0 +1,10 @@
+package apiprograms;
+
+public class Test1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi");
+	}
+
+}
---
+++ src/test/java/apiprograms/Test2.java
@@ -0,0 +1,10 @@
+package apiprograms;
+
+public class Test2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Bye");
+	}
+
+}
